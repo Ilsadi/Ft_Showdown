c011400be38e2973b891f9162ab7ab42d28d00e7
ilsadi <ilsadi@f2r7s9.paris.42.school>
2026-05-25 16:12:33 +0200

    oeoe

--- pokemon-arena/src/components/TeleLogo.jsx
+++ pokemon-arena/src/components/TeleLogo.jsx
@@ -7,22 +7,22 @@ export default function TeleLogo()
 	const posY = 2.
 	const posZ = -4.9
 
-	const fondW = 4     // largeur du fond gris
+	const fondW = 4.2     // largeur du fond gris
 	const fondH = 2 // hauteur du fond gris
 	const logoScale = 0.01
 
 	return (
 		<>
 			{/* FOND GRIS - rectangulaire, taille de la télé */}
-			<mesh position={[0, 2.65, -5.1]}>
+			<mesh position={[0, 2.76, -5.1]}>
 				<planeGeometry args={[fondW, fondH]} />
-				<meshBasicMaterial color="grey" />
+				<meshBasicMaterial color="#585858" />
 			</mesh>
 
 			{/* LOGO ANIMÉ - devant le fond */}
 			<AnimatedSprite
 				url="/logoMoving.gif"
-				position={[0, 2, -5]}
+				position={[0, 2.14, -5]}
 				scale={logoScale}
 				frameDuration={0.05}
 			/>
